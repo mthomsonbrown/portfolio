7ae29f8f7a2f455dde19ac3d46908a4a92b23bd0
Mike <mthomsonbrown@gmail.com>
2018-01-04 17:01:13 -0800

    Put in portfolio page and placeholder text in about section.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './core/app.component';
 import { AppRoutingModule } from './core/app-routing.module';
-import { HomeComponent } from './components/home/home.component'
+import { HomeComponent } from './components/home/home.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 import {
   MatToolbarModule,
-  MatButtonModule
+  MatButtonModule,
+  MatGridListModule,
+  MatCardModule
 } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PortfolioComponent
   ],
   imports: [
     AppRoutingModule,
     MatToolbarModule,
     MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
     BrowserModule
   ],
   providers: [],
--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -5,11 +5,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './../components/home/home.component';
+import { PortfolioComponent } from './../components/portfolio/portfolio.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent }
 ];
 
 @NgModule({
